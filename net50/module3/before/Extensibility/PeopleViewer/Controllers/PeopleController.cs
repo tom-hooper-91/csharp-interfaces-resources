e6719b1159ab8bd83871706b5ef229fae2a84b87
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-21 16:00:49 +1200

    implement ServiceReader for Web Service

--- net50/module3/before/Extensibility/PeopleViewer/Controllers/PeopleController.cs
+++ net50/module3/before/Extensibility/PeopleViewer/Controllers/PeopleController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PersonReader.Interface;
+using PersonReader.Service;
 using System.Collections.Generic;
 
 namespace PeopleViewer.Controllers
@@ -8,8 +9,12 @@ namespace PeopleViewer.Controllers
     {
         public IActionResult UseService()
         {
+            IPersonReader reader = new ServiceReader();
+            IEnumerable<Person> people = reader.GetPeople();
+
+            ViewData["ReaderType"] = reader.GetType().ToString();
             ViewData["Title"] = "Using a Web Service";
-            return View("Index", new List<Person>());
+            return View("Index", people);
         }
 
         public IActionResult UseCSV()
--- net50/module3/before/Extensibility/PersonReader.Interface/PersonReader.Service/ServiceReader.cs
+++ net50/module3/before/Extensibility/PersonReader.Interface/PersonReader.Service/ServiceReader.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
 using PersonReader.Interface;
 
 namespace PersonReader.Service
 {
     public class ServiceReader : IPersonReader
     {
+        WebClient client = new WebClient();
+        string baseUri = "https://localhost:9874";
+        JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
         public IEnumerable<Person> GetPeople()
         {
-            throw new NotImplementedException();
+            string address = $"{baseUri}/people";
+            string reply = client.DownloadString(address);
+            return JsonSerializer.Deserialize<IEnumerable<Person>>(reply, options); 
         }
 
         public Person GetPerson(int id)
         {
-            throw new NotImplementedException();
+            string address = $"{baseUri}/people/{id}";
+            string reply = client.DownloadString(address);
+            return JsonSerializer.Deserialize<Person>(reply, options);
+
         }
     }
 }
